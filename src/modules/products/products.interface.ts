04384966ac52c77329afbff6ee63f8ad6140ccbc
dat nguyen <datnguyenftu210@gmail.com>
2023-10-03 22:08:51 +0700

    feat(SCHEMA DESIGN): update product schema; add a product to the database

--- src/modules/products/products.interface.ts
+++ src/modules/products/products.interface.ts
@@ -7,3 +7,11 @@ export type ProductSpecs = {
   key: string;
   value: string;
 };
+
+export enum CountriesOfOrigin {
+  vietnam = 'VIỆT NAM',
+  japan = 'NHẬT BẢN',
+  malaysia = 'MALAYSIA',
+  thailand = 'THÁI LAN',
+  china = 'TRUNG QUỐC',
+}
--- src/modules/products/products.schema.ts
+++ src/modules/products/products.schema.ts
@@ -1,4 +1,8 @@
-import { CurrencyUnits, ProductSpecs } from './products.interface';
+import {
+  CountriesOfOrigin,
+  CurrencyUnits,
+  ProductSpecs,
+} from './products.interface';
 import * as mongoose from 'mongoose';
 
 export class ProductDocument {
@@ -6,13 +10,16 @@ export class ProductDocument {
   name: string;
   description: string;
   price: number;
-  productCode: number;
+  productCode: string;
   capacity: {
     amount: number;
     unit: { type: string; enum: CurrencyUnits };
   };
-  displayImg: string;
+  displayImgs: string[];
   specs: ProductSpecs[];
+  note: string;
+  origin: { type: string; enum: CountriesOfOrigin };
+  material: string;
 }
 
 export const ProductSchema = new mongoose.Schema(ProductDocument);
